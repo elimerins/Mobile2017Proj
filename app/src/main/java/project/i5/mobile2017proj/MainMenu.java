895dea79eaa5b887f8b952096949653a4c672461
yeongmin Jo <elimerins@gmail.com>
2017-05-06 01:44:19 +0900

    intro - mainmenu 구조로 바꿈

--- app/src/main/java/project/i5/mobile2017proj/MainMenu.java
+++ app/src/main/java/project/i5/mobile2017proj/MainMenu.java
@@ -1,13 +1,26 @@
 package project.i5.mobile2017proj;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainMenu extends AppCompatActivity {
+import project.i5.mobile2017proj.Map.MapMain;
+
+public class MainMenu extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        findViewById(R.id.button).setOnClickListener(this);
     }
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.button:
+                startActivity(new Intent(this, MapMain.class));;
+        }
+    }
+
 }
